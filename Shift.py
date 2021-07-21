3d20d6a0c7000dcb9515d5b7d20861dacf65ee47
Yulandia <tyakina88@gmail.com>
2021-07-20 23:25:57 -0500

    (Added employee_shift)

---
+++ Shift.py
@@ -0,0 +1,8 @@
+employee_shift = ["Mike", "Andrew", "Emma", "Kelly", "John", "Brad"]
+def replace_employee(employee_shift, old_employee, new_employee):
+    index_old_employee = employee_shift.index(old_employee)
+    employee_shift.remove(old_employee)
+    employee_shift.insert(index_old_employee, new_employee)
+    print(employee_shift)
+
+replace_employee(employee_shift, old_employee ="Kelly", new_employee = "Maria")
